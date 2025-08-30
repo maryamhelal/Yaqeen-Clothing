a18902bebf134734a67fd32426a80f866a30b7dd
maryamhelal <maryam.helaal22@gmail.com>
2025-08-30 19:27:56 +0300

    add preparing to frontend status

--- frontend/src/components/OrderManagement.jsx
+++ frontend/src/components/OrderManagement.jsx
@@ -115,6 +115,7 @@ export default function OrderManagement() {
           >
             <option value="">All Orders</option>
             <option value="pending">Pending</option>
+            <option value="preparing">Preparing</option>
             <option value="paid">Paid</option>
             <option value="shipped">Shipped</option>
             <option value="delivered">Delivered</option>
